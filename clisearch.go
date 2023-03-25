d9754f7696e7772edfe3163e36bd39188b66e071
davidbystrom <david_bystrom@live.se>
2023-03-25 16:33:22 +0100

    Create search function

---
+++ clisearch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExampleScrape() {
+	// Request the HTML page.
+	res, err := http.Get("https://google.com/search?q=q")
+	if err != nil {
+	  log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+	  log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+  
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	
+	numLinks := 1
+  
+	// Find the review items
+	doc.Find("a").EachWithBreak(func(_ int, s *goquery.Selection) bool {
+		  // For each item found, get the title
+		  url, ok := s.Attr("href")
+		  if ok {
+			if strings.Contains(url, "/url"){
+				fmt.Printf("Result %d: %s\n", numLinks, strings.Replace(url, "/url?q=", "", 1))
+				numLinks++
+			}
+		  }
+		  if numLinks == 11 {
+			return false
+		  }
+		  return true; 
+	  })
+  }
+  
+  func main() {
+	ExampleScrape()
+  }
