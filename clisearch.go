ea8fa13e8d9b75d5a02e672c7f4b60a5fb2fd307
davidbystrom <david_bystrom@live.se>
2023-03-25 17:14:27 +0100

    Create Test

--- clisearch.go
+++ clisearch.go
@@ -4,47 +4,50 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
+func Scrape(query string) {
 	// Request the HTML page.
-	res, err := http.Get("https://google.com/search?q=q")
+	res, err := http.Get("https://google.com/search?q=" + query)
 	if err != nil {
-	  log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-	  log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
-  
+
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-	  log.Fatal(err)
+		log.Fatal(err)
 	}
-	
+
 	numLinks := 1
-  
+
 	// Find the review items
 	doc.Find("a").EachWithBreak(func(_ int, s *goquery.Selection) bool {
-		  // For each item found, get the title
-		  url, ok := s.Attr("href")
-		  if ok {
-			if strings.Contains(url, "/url"){
-				fmt.Printf("Result %d: %s\n", numLinks, strings.Replace(url, "/url?q=", "", 1))
+		// For each item found, get the title
+		url, ok := s.Attr("href")
+		if ok {
+			if strings.Contains(url, "/url") {
+				fmt.Printf("Result %d: %s\n", numLinks, strings.Split(strings.Replace(url, "/url?q=", "", 1), "&")[0])
 				numLinks++
 			}
-		  }
-		  if numLinks == 11 {
+		}
+		if numLinks == 11 {
 			return false
-		  }
-		  return true; 
-	  })
-  }
-  
-  func main() {
-	ExampleScrape()
-  }
+		}
+		return true
+	})
+}
+
+func main() {
+	queryArray := os.Args[1:]
+	query := strings.Join(queryArray, "+")
+	Scrape(query)
+}
---
+++ clisearchTest.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestStatusCode(t *testing.T) {
+
+}
